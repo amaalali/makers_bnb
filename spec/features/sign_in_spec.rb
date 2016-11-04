3fcbb0153881523d2f94a529189644c34567297d
Tom Nunn <t@tomnunn.uk>
2016-11-04 17:30:01 +0000

    add further styling and markup

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -4,6 +4,6 @@ feature "Signing In" do
   scenario "User should be able to sign in and a welcome message be displayed" do
     sign_up
     sign_in_user
-    expect(page).to have_content 'Welcome, ben@actors.co.uk'
+    expect(page).to have_content 'Welcome, Ben Forest'
   end
 end
