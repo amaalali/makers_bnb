9f1139f39382221e6d30b30a735727b32c3c0f2a
Amaal Ali <amq.ali@gmail.com>
2016-11-04 11:02:23 +0000

    Restricting listing a space to signed in users 1) Tests added for listing a space 2) Signed out user:   .1) cannot see 'List a space' button on hamepage   .2) redirected from '/space/new' to '/session/new' to sign in

--- spec/features/list_a_space_spec.rb
+++ spec/features/list_a_space_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 feature 'List a Space' do
 
+  scenario 'Cannot click button "List a Space" if user is not logged in' do
+    visit '/'
+    expect(page).not_to have_content('List a Space')
+  end
+
+  scenario 'redirects to "Sign In" if user is not logged in' do
+    visit '/space/new'
+    expect(page).not_to have_content('List a Space')
+    expect(page).not_to have_content('Sign In')
+    # expect(page.current_path).to eq('/session/new')
+  end
+
   scenario 'create a new space' do
     sign_in_user
     list_a_space
